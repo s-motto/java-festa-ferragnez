236d9d5a2fa148df339ea41be4ee99589ec4140f
s-motto <brinamotto@gmail.com>
2024-06-10 15:57:58 +0200

    added array invitati

---
+++ com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,13 @@
+package com.ferragnez.party;
+import java.util.Scanner;
+public class CheckGuest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] invitati= {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		Scanner input = new Scanner(System.in);
+	
+	}
+
+}
